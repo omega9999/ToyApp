e9c4b17542fd1dfe4e1fefe5276ed7da013decab
Claudio <auron.tidus@gmail.com>
2019-10-09 22:14:58 +0200

    T02.02-Exercise-AddMenu

--- app/src/main/java/com/example/android/toyapp/MainActivity.java
+++ app/src/main/java/com/example/android/toyapp/MainActivity.java
@@ -1,43 +1,46 @@
 package com.example.android.toyapp;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mToysListTextView;
+    private EditText mSearchBoxEditText;
+
+    private TextView mUrlDisplayTextView;
+
+    private TextView mSearchResultsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-         * Using findViewById, we get a reference to our TextView from xml. This allows us to
-         * do things like set the text of the TextView.
-         */
-        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+        mSearchBoxEditText = findViewById(R.id.et_search_box);
 
-        /*
-         * This String array contains names of classic toys. After all, these are toy apps. We
-         * wanted to create a way to break concepts down into smaller pieces that we thought might
-         * be a little easier to understand. In each lesson, we'll demonstrate new concepts using a
-         * toy app (no, sadly every one won't have ACTUAL toys in it) and then we'll guide you
-         * through adding the functionality that you've just learned by having you use those
-         * concepts in Sunshine! Let us know what you think! We're really excited to have you
-         * taking this course.
-         */
-        String[] toyNames = ToyBox.getToyNames();
-
-        /*
-         * Iterate through the array and append the Strings to the TextView. The reason why we add
-         * the "\n\n\n" after the String is to give visual separation between each String in the
-         * TextView. Later, we'll learn about a better way to display lists of data.
-         */
-        for (String toyName : toyNames) {
-            mToysListTextView.append(toyName + "\n\n\n");
-        }
+        mUrlDisplayTextView = findViewById(R.id.tv_url_display);
+        mSearchResultsTextView = findViewById(R.id.tv_github_search_results_json);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_search){
+            Toast.makeText(this, "Search selected",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/example/android/toyapp/utilities/ToyBox.java
+++ app/src/main/java/com/example/android/toyapp/utilities/ToyBox.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.toyapp;
+package com.example.android.toyapp.utilities;
 
 public final class ToyBox {
 
