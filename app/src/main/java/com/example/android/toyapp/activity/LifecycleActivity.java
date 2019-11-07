2c61bb530769c496e17d28d0726c58b6aa64823e
Claudio <auron.tidus@gmail.com>
2019-11-07 22:40:24 +0100

    T05a.02-Exercise-PersistData

--- app/src/main/java/com/example/android/toyapp/activity/LifecycleActivity.java
+++ app/src/main/java/com/example/android/toyapp/activity/LifecycleActivity.java
@@ -13,7 +13,7 @@ import com.example.android.toyapp.R;
 
 public class LifecycleActivity extends AppCompatActivity {
 
-
+    private static final String LIFECYCLE_CALLBACKS_TEXT_KEY = "lifecycle_callbacks_text_key";
     /*
      * This tag will be used for logging. It is best practice to use the class's name using
      * getSimpleName as that will greatly help to identify the location from which your logs are
@@ -53,43 +53,97 @@ public class LifecycleActivity extends AppCompatActivity {
 
         mLifecycleDisplay = findViewById(R.id.tv_lifecycle_events_display);
 
+        if (savedInstanceState != null){
+            if (savedInstanceState.containsKey(LIFECYCLE_CALLBACKS_TEXT_KEY)){
+                mLifecycleDisplay.setText(savedInstanceState.getString(LIFECYCLE_CALLBACKS_TEXT_KEY));
+            }
+        }
+
         logAndAppend(ON_CREATE);
     }
 
+    /**
+     * Called when the activity is becoming visible to the user.
+     * <p>
+     * Followed by onResume() if the activity comes to the foreground, or onStop() if it becomes
+     * hidden.
+     */
     @Override
     protected void onStart() {
         super.onStart();
         logAndAppend(ON_START);
     }
 
+    /**
+     * Called when the activity will start interacting with the user. At this point your activity
+     * is at the top of the activity stack, with user input going to it.
+     * <p>
+     * Always followed by onPause().
+     */
     @Override
     protected void onResume() {
         super.onResume();
         logAndAppend(ON_RESUME);
     }
 
+    /**
+     * Called when the system is about to start resuming a previous activity. This is typically
+     * used to commit unsaved changes to persistent data, stop animations and other things that may
+     * be consuming CPU, etc. Implementations of this method must be very quick because the next
+     * activity will not be resumed until this method returns.
+     * <p>
+     * Followed by either onResume() if the activity returns back to the front, or onStop() if it
+     * becomes invisible to the user.
+     */
     @Override
     protected void onPause() {
         super.onPause();
         logAndAppend(ON_PAUSE);
     }
 
+    /**
+     * Called when the activity is no longer visible to the user, because another activity has been
+     * resumed and is covering this one. This may happen either because a new activity is being
+     * started, an existing one is being brought in front of this one, or this one is being
+     * destroyed.
+     * <p>
+     * Followed by either onRestart() if this activity is coming back to interact with the user, or
+     * onDestroy() if this activity is going away.
+     */
     @Override
     protected void onStop() {
         super.onStop();
         logAndAppend(ON_STOP);
     }
 
+    /**
+     * Called after your activity has been stopped, prior to it being started again.
+     * <p>
+     * Always followed by onStart()
+     */
     @Override
     protected void onRestart() {
         super.onRestart();
         logAndAppend(ON_RESTART);
     }
 
+    /**
+     * The final call you receive before your activity is destroyed. This can happen either because
+     * the activity is finishing (someone called finish() on it, or because the system is
+     * temporarily destroying this instance of the activity to save space. You can distinguish
+     * between these two scenarios with the isFinishing() method.
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
         logAndAppend(ON_DESTROY);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        logAndAppend(ON_SAVE_INSTANCE_STATE);
+        outState.putString(LIFECYCLE_CALLBACKS_TEXT_KEY, mLifecycleDisplay.getText().toString());
     }
 
 
@@ -102,7 +156,6 @@ public class LifecycleActivity extends AppCompatActivity {
      */
     private void logAndAppend(@NonNull final String lifecycleEvent) {
         Log.d(TAG, "Lifecycle Event: " + lifecycleEvent);
-
         mLifecycleDisplay.append(lifecycleEvent + "\n");
     }
 
