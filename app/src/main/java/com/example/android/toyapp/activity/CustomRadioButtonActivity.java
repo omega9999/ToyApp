ea65adfb48e0e8c49e95635e9584abb81bf28e37
Claudio <auron.tidus@gmail.com>
2020-01-21 21:54:29 +0100

    disabled button replaced with textview with listener

--- app/src/main/java/com/example/android/toyapp/activity/CustomRadioButtonActivity.java
+++ app/src/main/java/com/example/android/toyapp/activity/CustomRadioButtonActivity.java
@@ -31,30 +31,33 @@ public class CustomRadioButtonActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        final List<RadioButton> list = new ArrayList<>();
+        final List<View> list = new ArrayList<>();
         list.add(findViewById(R.id.radio1));
         list.add(findViewById(R.id.radio2));
         list.add(findViewById(R.id.radio3));
         list.add(findViewById(R.id.radio4));
 
-        disable(list.get(2));
+        list.get(2).setOnClickListener(v -> Toast.makeText(CustomRadioButtonActivity.this, "OnClickListener: Click on radio button disabled", Toast.LENGTH_SHORT).show());
+        //disable(list.get(2));
         hidden(list.get(1));
     }
 
+    /*
     //TODO implement better: greyscale and message when clicked, but do not change state
     private void disable(@NonNull final RadioButton radioButton) {
         final ColorMatrix matrix = new ColorMatrix();
         matrix.setSaturation(0);  //0 means grayscale
         final ColorMatrixColorFilter cf = new ColorMatrixColorFilter(matrix);
         radioButton.setEnabled(false);
-        radioButton.setClickable(true);
-        radioButton.setFocusable(true);
         radioButton.getBackground().setColorFilter(cf);
-        radioButton.setOnClickListener(v -> Toast.makeText(CustomRadioButtonActivity.this, "OnClickListener: Click on radio button disabled", Toast.LENGTH_SHORT).show());
+        //radioButton.setOnClickListener(v -> Toast.makeText(CustomRadioButtonActivity.this, "OnClickListener: Click on radio button disabled", Toast.LENGTH_SHORT).show());
+
         radioButton.setOnTouchListener((v,t) -> {Toast.makeText(CustomRadioButtonActivity.this, "OnTouchListener: Click on radio button disabled", Toast.LENGTH_SHORT).show();radioButton.performClick();return true;});
     }
 
-    private void hidden(@NonNull final RadioButton radioButton) {
+     */
+
+    private void hidden(@NonNull final View radioButton) {
         radioButton.setVisibility(View.GONE);
     }
 }
